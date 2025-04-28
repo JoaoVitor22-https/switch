7b9d0c5fa66c7bdae5bf6451aaa216ea875b4735
J.U.!.C.£ <joaovitor20071220@gmail.com>
2025-04-28 10:21:36 -0300

    pronto

---
+++ Switch/Program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite um numero de 1 a 7: ");
+        int informacao = Convert.ToInt32(Console.ReadLine());
+
+
+        switch (informacao)
+        {
+
+            case 1:
+                Console.WriteLine("Domingo");
+            break;
+                
+            case 2:
+                Console.WriteLine("Segunda");
+            break;
+
+            case 3:
+                Console.WriteLine("Terça");
+            break;
+
+            case 4:
+                Console.WriteLine("Quarta");
+            break;
+
+            case 5:
+                Console.WriteLine("Quinta");
+            break;
+
+            case 6:
+                Console.WriteLine("Sexta");
+            break;
+
+            case 7:
+                Console.WriteLine("Sabado");
+            break;
+
+            default:
+                Console.WriteLine("Valor não encontrado");
+            break;
+
+        }
+    }
+}
